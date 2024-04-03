1c25583a79b65eef6b3d30a8cc386ab33cd84d6f
Sergey <borisov11092000@gmail.com>
2024-04-04 01:05:53 +0300

    Initial commit

---
+++ Cointrunk/SceneDelegate.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+          guard let windowScene = (scene as? UIWindowScene) else { return }
+
+          window = UIWindow(windowScene: windowScene)
+          let viewController = ViewController()
+          window?.rootViewController = viewController
+          window?.makeKeyAndVisible()
+      }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    }
+}
+
